9df18eaad08c19f67d9f1c3b4b9a69c1170c9fc9
User <user@Users-iMac.local>
2025-01-31 15:50:01 +1300

    Initial Commit

---
+++ src/assets/assets.js
@@ -0,0 +1,16 @@
+
+import logo from './logo_adm.png'
+import add_icon from './add_icon.png'
+import order_icon from './order_icon.png'
+import profile_image from './profile_image.png'
+import upload_icon from './upload_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    add_icon,
+    order_icon,
+    profile_image,
+    upload_icon,
+    parcel_icon
+}
